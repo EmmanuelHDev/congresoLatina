f5791f2ec1f1458800e9084b7400df9e732a4bff
Emmanuel Herrera <emmanuelherrerab31@gmail.com>
2025-09-13 21:39:20 -0500

    subiendo el componente header

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Menu, X } from "lucide-react";
-import logoHeader from "../assets/logoHeader.png";
+
 
 export default function Header({ onLoginClick, onRegisterClick }) {
   const [menuOpen, setMenuOpen] = useState(false);
